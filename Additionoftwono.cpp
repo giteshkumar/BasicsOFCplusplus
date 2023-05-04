02d14f2c5c3f7530b51bedd87e0693ca016c668a
giteshkumar <mails2gitesh@gmail.com>
2023-05-05 01:17:26 +0530

    moresourcecode

---
+++ Additionoftwono.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+int a,b,sum;
+cout<<"Enter the value of a"<<endl;
+cin>>a;
+cout<<"enter the value of b"<<endl;
+cin>>b;
+
+sum = a+b;
+cout<<"sum of two numbers A and B Are: "<<sum<<endl;
+return 0;
+}
---
+++ SubstractionofTwono.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int a , b, sub;
+
+cout<<"enter the value of a"<<endl;
+cin>>a;
+cout<<"entEr the Value of b"<<endl;
+cin>>b;
+
+sub = a-b;
+
+cout<<"the value of a"<<a<<"the value of b"<<b<<" ="<<sub<<endl;
+return 0;
+
+}
+
